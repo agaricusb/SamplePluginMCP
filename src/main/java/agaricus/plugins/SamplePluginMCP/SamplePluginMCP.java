16b34b676e6c0d6cb365843f134301c97c886163
Agaricus <agaricusb@yahoo.com>
2013-02-21 18:50:12 -0800

    New project

---
+++ src/main/java/agaricus/plugins/SamplePluginMCP/SamplePluginMCP.java
@@ -0,0 +1,11 @@
+
+package agaricus.plugins.SamplePluginMCP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SamplePluginMCP extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Hello, Bukkit!");
+    }
+}
